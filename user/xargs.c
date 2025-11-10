824e83cb9386be698cec43fc676fe384b3c25f09
Nguyen Dinh Ha Duong <23122002@student.hcmus.edu.vn>
2025-11-10 10:01:14 +0700

    push xargs

---
+++ user/xargs.c
@@ -0,0 +1,44 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Usage: xargs command [args...]\n");
+        exit(1);
+    }
+
+    char buf[512];
+    char *xargv[MAXARG];
+    int n = 0; // số lượng ký tự đọc
+    int i;
+    
+    // Copy phần command gốc vào xargv
+    for (i = 1; i < argc; i++) {
+        xargv[i - 1] = argv[i];
+    }
+    int base = i - 1; // số lượng args gốc
+
+    // Đọc stdin ký tự một
+    while (read(0, &buf[n], 1) == 1) {
+        if (buf[n] == '\n') {
+            buf[n] = 0; // kết thúc chuỗi
+            xargv[base] = buf; // nối thêm đối số mới
+            xargv[base + 1] = 0;
+
+            // fork + exec
+            if (fork() == 0) {
+                exec(argv[1], xargv);
+                fprintf(2, "exec failed\n");
+                exit(1);
+            }
+            wait(0);
+            n = 0; // reset buffer
+        } else {
+            n++;
+        }
+    }
+
+    exit(0);
+}
--- user/xargstest.sh
+++ user/xargstest.sh
@@ -1,6 +1,12 @@
-mkdir a
-echo hello > a/b
-mkdir c
-echo hello > c/b
-echo hello > b
-find . b | xargs grep hello
+# user/xargstest.sh
+# Script test xargs cho xv6
+
+# Test đơn giản
+echo hello | xargs echo
+echo hello | xargs echo hello
+echo hello | xargs echo hello hello
+
+# Test nhiều dòng
+(echo hello; echo hello) | xargs echo
+(echo hello; echo hello) | xargs echo hello
+(echo hello; echo hello) | xargs echo hello hello
